58a47ce1fda2a6af2549a6f31e456d1e9da231d0
karunakar bhogyari <karunakar1008@gmail.com>
2023-03-05 13:24:33 +0530

    Connect terraform to azure using azure cli so that service principle  credentials are not required to save

---
+++ demo3/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+}
+
+# Authenticate using azure CLI
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-nextops-vedios-demo2"
+  location = "East Us"
+}
---
+++ demo3/readme.txt
@@ -0,0 +1,8 @@
+Learning: How to authenticate using azure cli
+
+1. Install azure cli
+2. az login
+3. az account set --subscription 460f276e-b221-479d-92d9-13dca26ead42 
+
+4. terraform init
+
